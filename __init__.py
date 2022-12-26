dcd2a99d3c63acb9613279ec009270fffc78345f
Abdo <abdo@abdnh.net>
2022-12-27 00:52:39 +0300

    Convert dialog to window

--- __init__.py
+++ __init__.py
@@ -37,7 +37,7 @@ def showTagsInfo(browser, cids, nids, highlights="", highlights_percent=50):
 
     class CardInfoDialog(QDialog):
         def __init__(self, parent) -> None:
-            super().__init__(parent)
+            super().__init__(parent, Qt.WindowType.Window)
             qconnect(self.finished, self.on_close)
 
         def on_close(self):
